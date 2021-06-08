52985f55a47903b3dc02e609ecd523f1bb322de2
Brian Angulo <brianangulo96@gmail.com>
2021-06-08 17:50:07 -0400

    Moving regex out of the jsx file

--- components/LoginView.js
+++ components/LoginView.js
@@ -9,7 +9,7 @@ import { setIsSignedIn } from "../redux/appSlice";
 function LoginView() {
   //redux
   const dispatch = useDispatch();
-  
+
   //regex email & pwd
   const emailRegex =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -40,19 +40,21 @@ function LoginView() {
     auth.signInWithEmailAndPassword(email, password).catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      Platform.OS === "ios" ? Alert.alert(errorCode) : ToastAndroid.show(errorCode, ToastAndroid.LONG);
+      Platform.OS === "ios"
+        ? Alert.alert(errorCode)
+        : ToastAndroid.show(errorCode, ToastAndroid.LONG);
       console.log(` errCode: ${errorCode} & errMess: ${errorMessage}`);
     });
   };
 
   //Submit login button handler!
   const handleSubmit = () => {
-     //checking to make sure all of the right information is being entered before submitting
-     //else alerting user
-     if (auth.currentUser !== null) {
-        Alert.alert("You are already signed in!");
-        return
-     } else if (
+    //checking to make sure all of the right information is being entered before submitting
+    //else alerting user
+    if (auth.currentUser !== null) {
+      Alert.alert("You are already signed in!");
+      return;
+    } else if (
       emailRegex.test(email) &&
       pwdRegex.test(password) &&
       email !== "" &&
@@ -75,6 +77,8 @@ function LoginView() {
       switchValue={switchValue}
       setSwitchValue={setSwitchValue}
       handleSubmit={handleSubmit}
+      emailRegex={emailRegex}
+      pwdRegex={pwdRegex}
     />
   );
 }
